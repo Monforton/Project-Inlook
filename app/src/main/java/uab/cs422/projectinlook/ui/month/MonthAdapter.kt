84e7c7feec6c36ee3622d375b0129b35e1282eee
Monforton <113210196+Monforton@users.noreply.github.com>
2023-04-20 01:05:52 -0500

    Month view now works

--- app/src/main/java/uab/cs422/projectinlook/ui/month/MonthAdapter.kt
+++ app/src/main/java/uab/cs422/projectinlook/ui/month/MonthAdapter.kt
@@ -3,29 +3,33 @@ package uab.cs422.projectinlook.ui.month
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import uab.cs422.projectinlook.R
 
 class MonthAdapter(
-    private val daysOfMonth: ArrayList<String>,
-    private val onItemListener: OnItemListener
-) : RecyclerView.Adapter<MonthViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonthViewHolder {
+    private val daysOfMonth: ArrayList<String>
+) : RecyclerView.Adapter<MonthAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) :
+        RecyclerView.ViewHolder(view) {
+        val dayOfMonth: TextView = view.findViewById(R.id.cellDayText)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view: View = inflater.inflate(R.layout.cell_month, parent, false)
         val layoutParams = view.layoutParams
         layoutParams.height = (parent.height * 0.166666666).toInt()
-        return MonthViewHolder(view, onItemListener)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: MonthViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.dayOfMonth.text = daysOfMonth[position]
     }
 
     override fun getItemCount(): Int {
         return daysOfMonth.size
-    }
-
-    interface OnItemListener {
-        fun onItemClick(position: Int, dayText: String?)
     }
 }
--- app/src/main/java/uab/cs422/projectinlook/ui/month/MonthFragment.kt
+++ app/src/main/java/uab/cs422/projectinlook/ui/month/MonthFragment.kt
@@ -5,11 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import uab.cs422.projectinlook.databinding.FragmentMonthBinding
@@ -22,51 +19,42 @@ import java.time.format.DateTimeFormatter
 class MonthFragment : Fragment(), CalendarInterface {
 
     private var _binding: FragmentMonthBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-    private var monthYearText: TextView? = null
-    private var monthRecyclerView: RecyclerView? = null
-    private var selectedDate: LocalDate? = null
+    private lateinit var monthRecyclerView: RecyclerView
+    private var selectedDate: LocalDate = LocalDateTime.now().toLocalDate()
+    private val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val monthViewModel =
-            ViewModelProvider(this).get(MonthViewModel::class.java)
-
         _binding = FragmentMonthBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        monthRecyclerView = binding.monthRecyclerView
 
-        val textView: TextView = binding.textMonth
-        monthViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        monthRecyclerView = binding.monthRecyclerView
+
+        setMonthView()
         return root
     }
 
-    private fun initWidgets() {
-        monthRecyclerView = findViewById<RecyclerView>(R.id.monthRecyclerView)
-        monthYearText = findViewById<TextView>(R.id.monthYearTV)
-    }
-
     private fun setMonthView() {
-        monthYearText!!.text = monthYearFromDate(selectedDate)
+        (context as AppCompatActivity).supportActionBar?.title =
+            selectedDate.format(formatter)
         val daysInMonth = daysInMonthArray(selectedDate)
-        val MonthAdapter = MonthAdapter(daysInMonth, this)
-        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(applicationContext, 7)
-        monthRecyclerView!!.layoutManager = layoutManager
-        monthRecyclerView!!.adapter = MonthAdapter
+        val monthAdapter = MonthAdapter(daysInMonth)
+        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this.requireContext(), 7)
+        monthRecyclerView.layoutManager = layoutManager
+        monthRecyclerView.adapter = monthAdapter
+        updateEvents()
     }
 
     private fun daysInMonthArray(date: LocalDate?): ArrayList<String> {
         val daysInMonthArray = ArrayList<String>()
         val yearMonth = YearMonth.from(date)
         val daysInMonth = yearMonth.lengthOfMonth()
-        val firstOfMonth = selectedDate!!.withDayOfMonth(1)
+        val firstOfMonth = selectedDate.withDayOfMonth(1)
         val dayOfWeek = firstOfMonth.dayOfWeek.value
         for (i in 1..42) {
             if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
@@ -78,23 +66,18 @@ class MonthFragment : Fragment(), CalendarInterface {
         return daysInMonthArray
     }
 
-    private fun monthYearFromDate(date: LocalDate?): String {
-        val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
-        return date!!.format(formatter)
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         (context as AppCompatActivity).supportActionBar?.title =
-            LocalDateTime.now().format(DateTimeFormatter.ofPattern("LLLL"))
+            LocalDateTime.now().format(formatter)
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
         (context as AppCompatActivity).supportActionBar?.title =
-            LocalDateTime.now().format(DateTimeFormatter.ofPattern("LLLL"))
+            LocalDateTime.now().format(formatter)
     }
 
     override fun onDestroyView() {
@@ -103,18 +86,19 @@ class MonthFragment : Fragment(), CalendarInterface {
     }
 
     override fun updateEvents() {
+
     }
 
     override fun onTodayButtonClicked() {
     }
 
-    override fun (view: View?) {
-        selectedDate = selectedDate!!.minusMonths(1)
+    override fun onSwipeLeft() {
+        selectedDate = selectedDate.plusMonths(1)
         setMonthView()
     }
 
-    override fun onSwipeRight(view: View?) {
-        selectedDate = selectedDate!!.plusMonths(1)
+    override fun onSwipeRight() {
+        selectedDate = selectedDate.minusMonths(1)
         setMonthView()
     }
 }
